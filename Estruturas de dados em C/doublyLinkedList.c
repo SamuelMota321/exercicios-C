f1dfc7d9290a9555caebdeee2ccfa78f84f6165e
Samuel Rodrigues da Mota <138218205+SamuelMota321@users.noreply.github.com>
2024-09-10 10:06:16 -0300

    Doubly Linked List

--- Estruturas de dados em C/doublyLinkedList.c
+++ Estruturas de dados em C/doublyLinkedList.c
@@ -5,6 +5,8 @@ typedef struct Node
 {
     int data;
     struct Node *next;
+    struct Node *prev;
+
 } Node;
 
 Node *createNode(int data);
@@ -13,30 +15,25 @@ void insertAtTail(Node **head, int data);
 void deleteNode(Node **head, int data);
 void printList(Node *head);
 
-int main()
+int main(void)
 {
     Node *head = NULL;
 
     insertAtHead(&head, 10);
     insertAtHead(&head, 20);
+    insertAtHead(&head, 30);
+    insertAtHead(&head, 25);
     insertAtTail(&head, 30);
     insertAtTail(&head, 40);
+    insertAtTail(&head, 50);
 
     printf("Lista apos insercoes:\n");
     printList(head);
 
-    deleteNode(&head, 20);
+    deleteNode(&head, 30);
 
-    printf("Lista apos deletar o no com valor 20:\n");
+    printf("Lista apos deletar o no com valor 30:\n");
     printList(head);
-
-    while (head != NULL)
-    {
-        Node *temp = head;
-        head = head->next;
-        free(temp);
-    }
-
     return 0;
 }
 
@@ -45,6 +42,7 @@ Node *createNode(int data)
     Node *newNode = (Node *)malloc(sizeof(Node));
     newNode->data = data;
     newNode->next = NULL;
+    newNode->prev = NULL;
     return newNode;
 }
 
@@ -52,61 +50,77 @@ void insertAtHead(Node **head, int data)
 {
     Node *newNode = createNode(data);
     newNode->next = *head;
+    if (*head != NULL)
+    {
+        (*head)->prev = newNode;
+    }
     *head = newNode;
 }
 
 void insertAtTail(Node **head, int data)
 {
-    Node *newNode = createNode(data);
+    struct Node *newNode = createNode(data);
+    struct Node *temp = *head;
+
     if (*head == NULL)
     {
         *head = newNode;
         return;
     }
-    Node *temp = *head;
+
     while (temp->next != NULL)
     {
         temp = temp->next;
     }
     temp->next = newNode;
+    newNode->prev = temp;
 }
 
 void deleteNode(Node **head, int data)
 {
-    Node *temp = *head;
-    Node *prev = NULL;
-
-    if (temp != NULL && temp->data == data)
+    if (*head == NULL)
     {
-        *head = temp->next;
-        free(temp);
         return;
     }
 
+    Node *temp = *head;
     while (temp != NULL && temp->data != data)
     {
-        prev = temp;
         temp = temp->next;
     }
 
     if (temp == NULL)
     {
-        printf("Valor não encontrado!\n");
         return;
     }
 
-    prev->next = temp->next;
+    if (*head == temp)
+    {
+        *head = temp->next;
+    }
+
+    if (temp->next != NULL)
+    {
+        temp->next->prev = temp->prev;
+    }
+
+    if (temp->prev != NULL)
+    {
+        temp->prev->next = temp->next;
+    }
     free(temp);
 }
 
 void printList(Node *head)
 {
     Node *temp = head;
+
+    printf("cabeca");
     while (temp != NULL)
     {
-        printf("%d -> ", temp->data);
+        printf(" %d ", temp->data);
         temp = temp->next;
     }
-    printf("NULL\n");
+    printf("cauda\n");
     free(temp);
 }
