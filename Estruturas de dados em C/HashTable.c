7c5296c10c33ac038653012d9e10537fbcbcb8f5
Samuel Rodrigues da Mota <138218205+SamuelMota321@users.noreply.github.com>
2024-09-18 17:39:14 -0300

    init hash table

---
+++ Estruturas de dados em C/HashTable.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// a tabela hash é uma estutura usada em casos quando é 
+// necessário uma busca rápida de um conjuto de elementos 
+// tendo uma complexidade O(1) constante para busca
+// geralmente na tabela hash não é feito muitas insersões 
+// e remoções, costuma-se saber qual será tamanho do 
+// conjunto de dados
+
+// tabela hash linear com vetor
+
+// para um vetor de 15 elementos  usamos a seguinte formula
+// para descobrir o tamanho da tabela
+// 2 * 15 = 30
+// observase que não usaremos o número 30, mas sim o primo 
+// mais próximo, podendo ser 29 ou 31
+
+int main (void) {
+    int tabela[] = {0};   
+    
+    return 0; 
+}
