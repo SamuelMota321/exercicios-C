110fb956dd13e9245956e8059c6033684b67b76a
SAMUEL RODRIGUES DA MOTA <samuel.rmota@a.ucb.br>
2024-09-20 13:51:04 -0300

    hashtable update

--- Estruturas de dados em C/HashTable.c
+++ Estruturas de dados em C/HashTable.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-// a tabela hash é uma estutura usada em casos quando é 
-// necessário uma busca rápida de um conjuto de elementos 
+// a tabela hash é uma estutura usada em casos quando é
+// necessário uma busca rápida de um conjuto de elementos
 // tendo uma complexidade O(1) constante para busca
-// geralmente na tabela hash não é feito muitas insersões 
-// e remoções, costuma-se saber qual será tamanho do 
+// geralmente na tabela hash não é feito muitas insersões
+// e remoções, costuma-se saber qual será tamanho do
 // conjunto de dados
 
 // tabela hash linear com vetor
@@ -14,11 +13,28 @@
 // para um vetor de 15 elementos  usamos a seguinte formula
 // para descobrir o tamanho da tabela
 // 2 * 15 = 30
-// observase que não usaremos o número 30, mas sim o primo 
+// observase que não usaremos o número 30, mas sim o primo
 // mais próximo, podendo ser 29 ou 31
+#define SIZE 31
 
-int main (void) {
-    int tabela[] = {0};   
-    
-    return 0; 
+void hashFuction(int table, int key);
+void insert(int data);
+
+int main(void)
+{
+    int table[SIZE] = {0};
+
+    return 0;
+}
+
+void hashFuction(int table[], int key)
+{
+    return key % SIZE;
+}
+
+void insert(int data)
+{
+    int id = hashFuction(data);
+    while (table[id] != 0)
+        id = hashFucton(id + 1);
 }
