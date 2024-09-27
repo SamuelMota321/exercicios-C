1195fa66c0febceb650a1c87450330e498e016a7
Samuel Rodrigues da Mota <138218205+SamuelMota321@users.noreply.github.com>
2024-09-27 08:25:50 -0300

    init binaryTree

---
+++ Estruturas de dados em C/BinarySearchTree.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//implemetação da arvore binária de busca
+
+typedef struct Node {
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+} Node;
+
+int main(void) {
+    
+    return 0;
+}
